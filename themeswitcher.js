c515e2220542917efaa7863e2adecd7f53f5d755
honorash <105509298+honorash@users.noreply.github.com>
2022-05-28 12:48:52 +0100

    Update themeswitcher.js

--- themeswitcher.js
+++ themeswitcher.js
@@ -16,14 +16,14 @@ for (let i of switches) {
 }
 
 function setTheme(theme) {
-  if (theme == 'high') {
-    document.getElementById('switcher-id').href = './themes/high.css';
-  } else if (theme == 'calm') {
+  if (theme == 'calm') {
     document.getElementById('switcher-id').href = './themes/calm.css';
   } else if (theme == 'dark') {
     document.getElementById('switcher-id').href = './themes/dark.css';
   } else if (theme == 'color') {
     document.getElementById('switcher-id').href = './themes/color.css';
+  } else if (theme == 'high') {
+    document.getElementById('switcher-id').href = './themes/high.css';
   }
   localStorage.setItem('style', theme);
 }
